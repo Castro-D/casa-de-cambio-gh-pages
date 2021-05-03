fdcef757c7ef8d40506be5ba2ed24e5d38fec6e4
Castro-D <castro.dh91@gmail.com>
2021-05-02 22:39:37 -0300

    add files

---
+++ main.js
@@ -0,0 +1,31 @@
+/// <reference types='jquery' />
+
+let $table = $('table');
+const $inputFecha = document.querySelector('#fecha');
+
+document.querySelector('button').onclick = function (){
+  eliminarBordeRojo()
+  resetearTabla()
+  const $moneda = document.querySelector('#bases').value;
+  const $fecha = document.querySelector('#fecha').value;
+  if ($fecha == ""){
+    $inputFecha.classList.add('borde-rojo')
+    return ''
+  }
+  fetch(`https://api.ratesapi.io/api/${$fecha}?base=${$moneda}`)
+    .then(response => response.json())
+    .then(data =>{
+      Object.keys(data.rates).forEach(function(key){
+        $table.append(`<tr><td>${key}</td><td>${data.rates[key]}</td></tr>`)
+      })
+    })
+    .catch(error => console.error("FALLO", error))
+}
+
+function resetearTabla(){
+  $table.find("tr:gt(0)").remove();
+}
+
+function eliminarBordeRojo(){
+  $inputFecha.classList.remove("borde-rojo");
+}
